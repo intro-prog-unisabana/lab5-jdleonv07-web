d32ef44b43d3178b34395ecfce8cfa95ac6c7822
Juan Diego <jd.leonv07@gmail.com>
2026-03-17 23:35:58 -0500

    prueba 5

---
+++ utils_calc.py
@@ -0,0 +1,29 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero is not allowed."
+    return a / b
+
+def exponent(a, b):
+    return a ** b
+
+def modulo(a, b):
+    if b == 0:
+        return "Error: Modulo by zero is not allowed."
+    return a % b
+
+def floor_divide(a, b):
+    if b == 0:
+        return "Error: Division by zero is not allowed."
+    return a // b
+
+def absolute(a):
+    return abs(a)
