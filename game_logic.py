aaf289ae83e7bff3efacd19a38fb5664284fd330
Juan Diego <jd.leonv07@gmail.com>
2026-03-17 23:19:36 -0500

    prueba 4

--- game_logic.py
+++ game_logic.py
@@ -3,11 +3,12 @@ from response import input_response
 seed = int(input("Enter a seed number:"))
 seed_secret_numbers(seed)
 correcto = False
-tries =0
+tries = 0
+generated_value = generate_secret_number()
 while correcto != True:
-    generated_value = generate_secret_number()
     user_input = int(input("What is your guess:"))
-    mensaje, correcto= input_response(generated_value, user_input)
+    mensaje, correcto = input_response(generated_value, user_input)
     print(mensaje)
     tries += 1
-    
+
+print(f"It took you {tries} tries!")
